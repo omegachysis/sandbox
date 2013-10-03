d7910200696d7f04997f41be1dbfc5b7709174ff
Matthew Robinson <omegachysis@outlook.com>
2013-10-03 16:54:53 -0400

    Added Shebang examples

---
+++ Python/Shebang/Script-2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+def main():
+    print raw_input("Pick your poison: ")
+    raw_input("Press enter to exit..")
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Shebang/Script-3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+    print(input("Pick your poison: "))
+    input("Press enter to exit..")
+
+if __name__ == "__main__":
+    main()
