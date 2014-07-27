b21cfec471f3b48f148685b6467a2a6b55e66c6a
Matthew Robinson <omegachysis@outlook.com>
2014-07-27 19:44:48 -0400

    Finished example 3

--- IronPython/WindowsForms/WindowsForms03/Part03.py
+++ IronPython/WindowsForms/WindowsForms03/Part03.py
@@ -18,8 +18,29 @@ from System.Windows.Forms import *
 
 class MyForm(Form):
     def __init__(self):
-        pass
+        self.Text = "Hello World"
+        self.Name = "Hello World"
 
+        label = Label()
+        label.Text = "Please Click Me"
+        label.Location = Point(50, 50)
+        label.Width = 200
+        label.Height = 30
+        self.Controls.Add(label)
+        self.label = label
+
+        button = Button()
+        button.Text = "Click Me"
+        button.Location = Point(50, 100)
+        button.Click += self.buttonPressed
+        self.Controls.Add(button)
+        self.button = button
+
+        self.count = 0
+
+    def buttonPressed(self, sender, args):
+        self.count += 1
+        self.label.Text = "You have clicked me %s times." % self.count
 
 Application.EnableVisualStyles()
 Application.SetCompatibleTextRenderingDefault(False)
